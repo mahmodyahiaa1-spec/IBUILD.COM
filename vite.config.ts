89c5d05d114a9a9c071cd257a04d755298da3b4e
Mahmoud Yahia <mahmodyahiaa1@example.com>
2026-06-14 22:02:46 +0300

    fix: reset configuration to allow vercel SSR

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,3 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  // احذف إعدادات السيرفر أو اجعلها فارغة
-  tanstackStart: {
-    server: { 
-      entry: "client", // تغيير الـ entry إلى client بدل server
-    },
-  },
-  build: {
-    outDir: 'dist',
-  }
-});
+export default defineConfig({});
