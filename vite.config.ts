cdc7510697f1c78e8fc8ce8f456470c26948feb2
Mahmoud Yahia <mahmodyahiaa1@example.com>
2026-06-14 22:19:46 +0300

    fix: factory reset to use native vercel build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,2 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-
-export default defineConfig({
-  tanstackStart: {
-    ssr: false // السطر السحري اللي هيحل الأزمة ويجبره يطلع ملفات ثابتة
-  }
-});
+export default defineConfig({});
